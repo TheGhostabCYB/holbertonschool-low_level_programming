4f714354ad2c0bb6944a081b709b3d83405f7b40
root <root@MSI.>
2025-02-18 10:33:54 +0100

    hopes

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point of the program
  *
- *Return:Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int n;
-srand(time(0))
+srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
