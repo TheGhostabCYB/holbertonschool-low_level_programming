0e78e4da811d11c240639df0340c7f95dfd621ed
root <root@MSI.>
2025-02-18 16:04:04 +0100

    lol

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)h
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
