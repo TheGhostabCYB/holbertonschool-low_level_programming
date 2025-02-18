5b7618534a15d34bd25b034618a00e1a972a1707
root <root@MSI.>
2025-02-18 10:59:52 +0100

    hoping

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,25 +3,25 @@
 #include <stdio.h>
 /**
  * main - Entry point of the program
- * 
- * Return:Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int n;
 srand(time(0));
-n = rand() - RAND_MAX / 2;
+n = (rand() - RAND_MAX / 2) % 10;
 if (n >= 5)
 {
-printf("n is greater than or equal to 5\n");
+printf("The last digit is greater than or equal to 5\n");
 }
 else if (n == 0)
 {
-printf("n is equal to 0\n");
+printf("The last digit is equal to 0\n");
 }
 else if (n < 5 && n != 0)
 {
-printf("n is less than 5 and not equal to 0\n");
+printf("The last digit is less than 5 and not equal to 0\n");
 }
-return 0;
+return (0);
 }
