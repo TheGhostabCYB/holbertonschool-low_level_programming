f81276723c4f292043aeaf28ff1c61436eba3aee
root <root@MSI.>
2025-02-18 15:53:05 +0100

    LA BMW A LE FEU AU CUL

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
  * 
@@ -9,24 +8,21 @@
  */
 int main(void)
 {
-    int n;
-    srand(time(0));
-    n = (rand() - RAND_MAX / 2) % 10;  // Get the last digit
-
-    printf("Last digit of %d is ", n);
-
-    if (n > 5)
-    {
-        printf("and is greater than 5\n");
-    }
-    else if (n == 0)
-    {
-        printf("and is 0\n");
-    }
-    else if (n < 6 && n != 0)
-    {
-        printf("and is less than 6 and not 0\n");
-    }
-
-    return 0;
+int n;
+srand(time(0));
+n = (rand() - RAND_MAX / 2) % 10;  // Get the last digit
+printf("Last digit of %d is ", n);
+if (n > 5)
+{
+printf("and is greater than 5\n");
+}
+else if (n == 0)
+{
+printf("and is 0\n");
+}
+else if (n < 6 && n != 0)
+{
+printf("and is less than 6 and not 0\n");
+}
+ return (0);
 }
