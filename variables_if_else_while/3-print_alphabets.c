f338b292b0c45a4107cbe41fc17956a6e4190096
root <root@MSI.>
2025-02-19 09:39:23 +0100

    one and only

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Description: This program displays the alphabet using putchar
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char char_var = 'a';
-char char_var2 = 'A';
-for (; char_var <= 'z'; char_var++)
-}
-putchar(char_var);
-}
-for (; char_var <= 'Z'; char_var2++)
-}
-putchar(char_var2);
-}
-printf("\n");
-return (0);
+    char char_var = 'a';
+    char char_var2 = 'A';
+    
+    for (; char_var <= 'z'; char_var++)
+    {
+        putchar(char_var);
+    }
+
+    for (; char_var2 <= 'Z'; char_var2++)
+    {
+        putchar(char_var2);
+    }
+
+    putchar('\n');
+
+    return (0);
 }
