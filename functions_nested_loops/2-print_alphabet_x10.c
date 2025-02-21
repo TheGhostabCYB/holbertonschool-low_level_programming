bb7f78b3f8db4f3984e0170dfed0b3c87775d748
root <root@MSI.>
2025-02-21 14:23:24 +0100

    lol

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,42 +1,21 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
- * _putchar - Writes a character to stdout
- * @c: The character to print
- * Return: On success 1, -1 on error
+ * print_alphabet_x10 - Entry point
+ * Return: no return
  */
-int _putchar(char c)
-{
-return (0);
-}
-/**
- * print_alphabet - Function to print the alphabet in lowercase
- */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char ch;
+int compteur;
+compteur = 0;
+while (compteur < 10)
+{
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 _putchar(ch);
 }
 _putchar('\n');
+compteur++;
 }
-/**
- * print_alphabet_x10 - Function to print the alphabet 10 times
- */
-void print_alphabet_x10(void)
-{
-for (int i = 0; i < 10; i++)
-{
-print_alphabet();
-}
-}
-/**
- * main - Entry point of the program
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-print_alphabet_x10();
-return (0);
 }
