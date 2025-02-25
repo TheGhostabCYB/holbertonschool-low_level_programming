8f594f8404a7957b30c4cee7e4e35403c9d3d61c
root <root@MSI.>
2025-02-25 15:07:32 +0100

    lol

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * print_numbers - print some digits
- *
- * Return: nothing
+ * print_numbers - print numbers
+ * Description: This function print numbers from
+ * 0 to 9
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	int num;
+	int i = 0;
 
-	for (num = 0; num <= 9; num++)
+	for (; i < 10; i++)
 	{
-		_putchar(num + '0');
+		_putchar('0' + i);
 	}
+
 	_putchar('\n');
 }
