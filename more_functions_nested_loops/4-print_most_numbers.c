ed54067bb0121e3777557b4f822d2ff2366d7cc4
root <root@MSI.>
2025-02-25 15:41:44 +0100

    zebi

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,23 @@
 #include "main.h"
+
 /**
- * print_numbers - print numbers
- * Description: This function prints numbers from
- * 0 to 9 (excluding 2 and 4)
+ * print_most_numbers - print numbers
+ * Description: This function will print all numbers from
+ * 0 to 9, excluding 2 and 4
  * Return: void
  */
-void print_numbers(void)
+
+void print_most_numbers(void)
 {
-int i = 0;
-for (; i < 10; i++)
-{
-if (i != 2 && i != 4)
-{
-_putchar('0' + i);
-}
-}
-_putchar('\n');
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+		i++;
+	}
+	_putchar('\n');
 }
