9ac09e740bd9e7678e2647cb3226fae483d8ba7c
root <root@MSI.>
2025-02-25 14:50:01 +0100

    no sur

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -8,5 +8,5 @@ void print_numbers(int n)
 if (n > 9)
 return;  /* Base case to stop recursion */
 putchar(n + '0');  /* Convert integer to character */
-print_number(n + 1);  /* Recursive call for the next number */
+print_numbers(n + 1);  /* Recursive call for the next number */
 }
