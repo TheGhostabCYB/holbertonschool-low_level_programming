2a85fecd55433e3b8d142bccc50f42d9e0e9624e
root <root@MSI.>
2025-02-25 10:16:22 +0100

    louche

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
-#include <ctype.h>
+#include "main.h"
 /**
- * main - check the code.
- * Return: Always 0.
+ * _isupper - checks for uppercases
+ * @c: a character
+ * Return: 1 if uppercase, else 0
  */
-int main()
+int _isupper(int c)
 {
-char c;
-c = 'A';
-printf("%c: %d\n", c, isupper(c));   
-c = 'a';
-printf("%c: %d\n", c, isupper(c));
-if (isupper(c))
-printf("\nEntered character is uppercase character");
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
 else
-printf("\nEntered character is not uppercase character");
+{
 return (0);
+}
 }
