c9c257449deac853a6296f37dcca80edaf908eef
root <root@MSI.>
2025-02-25 10:49:46 +0100

    hoping

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digits
+ * @c: a character
+ * Return: 1 if digit, else 0
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
