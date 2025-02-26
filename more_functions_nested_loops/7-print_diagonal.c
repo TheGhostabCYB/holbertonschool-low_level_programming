ad6837a677846f965a12b0aefa30c74b0e7e8ce8
root <root@MSI.>
2025-02-26 14:24:11 +0100

    i dont know how to park

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,38 @@
 #include "main.h"
+
 /**
  * print_diagonal - print a diagonal
  * @n: number of "\"
  * Return: nothing
  */
+
 void print_diagonal(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-return;
-}
-for (int i = 0; i < n; i++)
-{
-for (int j = 0; j < i; j++)
-{
-_putchar(' ');
-}
-_putchar('\\');
-_putchar('\n');
-}
+	int a;
+	int s;
+
+	a = 0;
+	s = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a != n; a++)
+		{
+			if (a != 0)
+			{
+				while (s < a)
+				{
+				_putchar(' ');
+				s++;
+				}
+			s = 0;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
