e03f4bbd441e3d6c553ead29eeaf9cde1239da04
root <root@MSI.>
2025-02-26 16:25:08 +0100

    lol

---
+++ pointers_array_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * update_to_98 - updates the value of an integer to 98
+ * @n: pointer to an integer
+ * Return: nothing
+ */
+void update_to_98(int *n)
+{
+    *n = 98;
+}
