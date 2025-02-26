b86ea7ba6e95e10b0d72b7c28da0e2c9dad71aa9
root <root@MSI.>
2025-02-26 16:28:57 +0100

    logique

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * update_to_98 - updates the value of an integer to 98
+ * reset_to_98 - reset the value of an integer to 98
  * @n: pointer to an integer
  * Return: nothing
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+*n = 98;
 }
