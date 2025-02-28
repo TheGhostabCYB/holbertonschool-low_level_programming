7bcef081efbbf07b53e3176bec76b0c2aef7661f
root <root@MSI.>
2025-02-28 09:44:13 +0100

    lol

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,18 +3,25 @@
  * _strcat - Concatenates two strings
  * @dest: Destination string to which src will be appended
  * @src: Source string to append to dest
+ *
  * Return: Pointer to the resulting string dest
  */
-char *_strcat(char *dest, char *src) {
+char *_strcat(char *dest, char *src)
+{
 char *ptr = dest;
-while (*ptr != '\0') {
+/* Move the pointer to the end of the dest string */
+while (*ptr != '\0')
+{
 ptr++;
 }
-while (*src != '\0') {
+/* Append the src string to dest */
+while (*src != '\0')
+{
 *ptr = *src;
 ptr++;
 src++;
 }
+/* Null-terminate the concatenated string */
 *ptr = '\0';
 return (dest);
 }
